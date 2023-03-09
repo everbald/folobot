bf48d6c167250631c9e66d11d7cb0fdae308b2f7
everbald <aravin.roman@gmail.com>
2023-03-09 14:04:22 +0300

    smalltalk suspend for foloswarm

--- src/main/kotlin/com/telegram/folobot/service/handlers/SmallTalkHandler.kt
+++ src/main/kotlin/com/telegram/folobot/service/handlers/SmallTalkHandler.kt
@@ -1,23 +1,42 @@
 package com.telegram.folobot.service.handlers
 
+import com.telegram.folobot.extensions.getChatIdentity
+import com.telegram.folobot.extensions.isFromFoloSwarm
 import com.telegram.folobot.service.OpenAIService
 import mu.KLogging
 import org.springframework.stereotype.Component
 import org.telegram.telegrambots.meta.api.methods.BotApiMethod
 import org.telegram.telegrambots.meta.api.objects.Update
+import java.util.*
+import kotlin.concurrent.schedule
+import kotlin.time.Duration
+import kotlin.time.Duration.Companion.seconds
 
 @Component
 class SmallTalkHandler(
     private val openAIService: OpenAIService
 ) : KLogging() {
-    /**
-     * Ответ на обращение
-     *
-     * @param update [Update]
-     * @return [BotApiMethod]
-     */
+    private var smallTalkStatus: MutableMap<Long?, Boolean> = mutableMapOf()
     fun handle(update: Update, withSetup: Boolean = true): BotApiMethod<*>? {
-        openAIService.smallTalk(update, withSetup)
+        if (update.message.isFromFoloSwarm()) {
+            if (smallTalkStatus[update.message?.chatId] != false) {
+                openAIService.smallTalk(update, withSetup)
+                suspend(update, 30.seconds)
+            } else {
+                logger.info { "Canceling small talk BC it's suspended in ${getChatIdentity(update.message.chatId)}" }
+            }
+        } else openAIService.smallTalk(update, withSetup)
         return null
+    }
+
+    private fun suspend(update: Update, duration: Duration) {
+        smallTalkStatus[update.message.chatId] = false
+        logger.info { "Small talk on repost is suspended for ${duration.inWholeSeconds} seconds in " +
+            getChatIdentity(update.message.chatId)
+        }
+        Timer().schedule(duration.inWholeMilliseconds) {
+            smallTalkStatus[update.message.chatId] = true
+            logger.info { "Small talk on repost is resumed in ${getChatIdentity(update.message.chatId)}" }
+        }
     }
 }
