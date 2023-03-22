0b1d948c8c0f559341eee575e6ff4ca78c963078
everbald <aravin.roman@gmail.com>
2023-03-22 23:15:09 +0300

    test folotrade

--- src/main/kotlin/com/everbald/folobot/service/folocoin/InvoiceService.kt
+++ src/main/kotlin/com/everbald/folobot/service/folocoin/InvoiceService.kt
@@ -66,7 +66,7 @@ class InvoiceService(
         val price = (foloCoinService.getPrice() * 100).toInt()
         return listOf(
             LabeledPrice(Product.FOLOCOIN.label, price),
-            LabeledPrice("Вы восхитительны! (скидка 20%)", price / 100 * -20),
+            LabeledPrice("Вы восхитительны! (-20%)", price / 100 * -20),
 //            LabeledPrice("Распродажа в честь открытия торгов (скидка 10%)", price / 100 * -10)
         )
     }
