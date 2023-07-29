6f4c90a6f607195fcc77c19cce00a8a4581285d6
Moon Moon <aravin.roman@gmail.com>
2023-07-29 17:28:11 +0300

    chore

--- src/main/kotlin/com/everbald/folobot/service/handlers/message/CommandHandler.kt
+++ src/main/kotlin/com/everbald/folobot/service/handlers/message/CommandHandler.kt
@@ -6,7 +6,6 @@ import com.everbald.folobot.model.Action
 import com.everbald.folobot.model.BotCommand
 import com.everbald.folobot.service.CommandService
 import com.everbald.folobot.service.MessageService
-import com.everbald.folobot.service.UserService
 import jakarta.annotation.Priority
 import org.springframework.stereotype.Component
 import org.telegram.telegrambots.meta.api.objects.EntityType
@@ -19,8 +18,7 @@ class CommandHandler(
     private val commandService: CommandService,
     private val messageService: MessageService,
     private val smallTalkHandler: SmallTalkHandler,
-    private val botCredentials: BotCredentialsConfig,
-    private val userService: UserService
+    private val botCredentials: BotCredentialsConfig
 ) : AbstractMessageHandler() {
     fun Message.isMyCommand() =
         this.isCommand && this.isNotForward() &&
