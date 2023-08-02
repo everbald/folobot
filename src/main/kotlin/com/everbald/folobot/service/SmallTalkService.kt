8a4ac3fcca85791011999e955f720819facf7177
Moon Moon <aravin.roman@gmail.com>
2023-08-02 21:14:36 +0300

    fix transcription

--- src/main/kotlin/com/everbald/folobot/service/SmallTalkService.kt
+++ src/main/kotlin/com/everbald/folobot/service/SmallTalkService.kt
@@ -45,14 +45,19 @@ class SmallTalkService(
 
     @OptIn(BetaOpenAI::class)
     fun transcription(update: Update) {
-        val fileSource: FileSource? = when {
-            update.message.hasVoice() || update.message.hasVideoNote() -> {
-                val target = fileService.downloadFileAsStream(update)
-                target?.let { FileSource(name = "file", source = it.source()) }
-            }
+        when {
+            update.message.hasVoice() -> "ogg"
+            update.message.hasVideoNote() -> "mp4"
             else -> null
-        }
-        fileSource?.let {
+        }?.let { extension ->
+            fileService.downloadFileAsStream(update)
+                ?.let {
+                    FileSource(
+                        name = "file.$extension",
+                        source = it.source()
+                    )
+                }
+        }?.let { fileSource ->
             val request = TranscriptionRequest(
                 audio = fileSource,
                 model = ModelId("whisper-1"),
@@ -62,13 +67,11 @@ class SmallTalkService(
         }
     }
 
-    private fun buildPrompt(message: Message): String? {
-        return when {
+    private fun buildPrompt(message: Message): String? =
+        when {
             message.hasAudio() -> null // TODO text from audio
             else -> message.preparePrompt()
         }
-
-    }
 
     @OptIn(DelicateCoroutinesApi::class, BetaOpenAI::class)
     private fun makeRequest(request: ChatCompletionRequest, update: Update) = GlobalScope.launch {
