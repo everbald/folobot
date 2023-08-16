c8500a65ae95ac91599f43000c738dae5512c932
Moon Moon <aravin.roman@gmail.com>
2023-08-16 16:29:22 +0300

    chore

--- src/main/kotlin/com/everbald/folobot/service/folocoin/model/OrderInfoDto.kt
+++ src/main/kotlin/com/everbald/folobot/service/folocoin/model/OrderInfoDto.kt
@@ -3,7 +3,6 @@ package com.everbald.folobot.service.folocoin.model
 import com.everbald.folobot.extensions.toObject
 import com.everbald.folobot.persistence.entity.OrderInfoEntity
 import org.telegram.telegrambots.meta.api.objects.payments.SuccessfulPayment
-import java.io.Serializable
 
 class OrderInfoDto(
     val id: Int? = null,
@@ -11,11 +10,8 @@ class OrderInfoDto(
     var status: OrderStatus,
     val payment: SuccessfulPayment,
     val payload: InvoicePayload = payment.invoicePayload.toObject()
-) : Serializable {
-    fun setStatus(status: OrderStatus): OrderInfoDto {
-        this.status = status
-        return this
-    }
+) {
+    fun setStatus(status: OrderStatus): OrderInfoDto = this.also { it.status = status }
 }
 
 fun OrderInfoDto.toEntity() = OrderInfoEntity(id, userId, status, payment)
