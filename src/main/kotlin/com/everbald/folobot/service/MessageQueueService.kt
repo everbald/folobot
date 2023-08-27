5219a173a772bc422835453752e558e7ea9faa52
Moon Moon <aravin.roman@gmail.com>
2023-08-28 01:48:25 +0300

    restore message fix

--- src/main/kotlin/com/everbald/folobot/service/MessageQueueService.kt
+++ src/main/kotlin/com/everbald/folobot/service/MessageQueueService.kt
@@ -50,10 +50,10 @@ class MessageQueueService(
 
 
     fun restoreMessages() {
-        return
+        return //FIXME при накоплении большого количества сообщений в стаке тг начинает выдвавать ошибку что слишком много запросов
         messageStack.addAll(messageQueue)
         messageQueue.clear()
-        messageStack.removeIf { it.recievedAt < LocalDateTime.now().minusDays(1) }
+        messageStack.removeIf { it.recievedAt < LocalDateTime.now().minusDays(1) || it.restored }
 
         messageStack.filter { it.message.chat.isFolochat() && !it.restored }
             .forEach { queueMessage ->
