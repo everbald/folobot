f630b27aedbaac9a8713000c2add9a36a93cf526
everbald <aravin.roman@gmail.com>
2024-04-04 14:45:21 +0300

    database startup validator

--- src/main/kotlin/com/everbald/folobot/Application.kt
+++ src/main/kotlin/com/everbald/folobot/Application.kt
@@ -5,12 +5,14 @@ import org.jetbrains.exposed.spring.autoconfigure.ExposedAutoConfiguration
 import org.springframework.boot.autoconfigure.ImportAutoConfiguration
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.jdbc.support.DatabaseStartupValidator
 
 @SpringBootApplication
 @ImportAutoConfiguration(ExposedAutoConfiguration::class)
 class Application
 
 fun main(args: Array<String>) {
-    runApplication<Application>(*args)
+    runApplication<Application>(DatabaseStartupValidator::class.java.name, *args)
+
 }
 
